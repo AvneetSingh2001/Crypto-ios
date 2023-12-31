904d7c0505c1fe660f53550275863fd15b7a78dd
Avneet Singh <avneetmaankiya@gmail.com>
2024-01-01 00:25:48 +0530

    Initial Commit

---
+++ Crypto ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Crypto ios
+//
+//  Created by Avi manak on 01/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Crypto ios/Crypto_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_iosApp.swift
+//  Crypto ios
+//
+//  Created by Avi manak on 01/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

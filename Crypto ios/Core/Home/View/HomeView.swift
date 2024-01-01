6289e2b65ca283768019ad9fa73742ff1de27bc2
Avneet Singh <avneetmaankiya@gmail.com>
2024-01-01 05:49:02 +0530

    nav header init

--- Crypto ios/Core/Home/View/HomeView.swift
+++ Crypto ios/Core/Home/View/HomeView.swift
@@ -8,11 +8,46 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @State private var showPortfolio: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            // background layer
+            Color.theme
+                .background
+                .ignoresSafeArea()
+
+            // Content layer
+            VStack {
+                HStack {
+                    CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                        .animation(nil, value: UUID())
+                    Spacer()
+                    Text("Live Prices")
+                        .font(.headline)
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.theme.accent)
+                    Spacer()
+                    CircleButtonView(iconName: "chevron.right")
+                        .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                        .onTapGesture {
+                            withAnimation(.spring()) {
+                                showPortfolio.toggle()
+                            }
+                        }
+                }
+                .padding(.horizontal)
+                
+                Spacer(minLength: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/)
+            }
+            
+        }
     }
 }
 
 #Preview {
-    HomeView()
+    NavigationView {
+        HomeView().navigationBarHidden(true)
+    }
 }
